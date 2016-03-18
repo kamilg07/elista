b559bb169e1db8ba9385e28fc92bc126f79e1f2c
kamilg07 <kamilg0700@gmail.com>
2016-03-18 09:35:12 +0100

    problem ze szwagrem, nie dodałem @ApiModel

--- src/main/java/elista/SwaggerConfig.java
+++ src/main/java/elista/SwaggerConfig.java
@@ -1,18 +1,35 @@
 package elista;
 
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+
+/**
+ * Created by Ja on 2016-03-18.
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	@Bean
-	public Docket userApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().paths(apiPaths()).build();
-	}
-	private Predicate<String> apiPaths() {
-		return or(regex("/elista/.*"));
-	}
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("E-Lista").description("Opis").termsOfServiceUrl("Terms of Service")
-				.contact("Dane kontaktowe").license("Apache License Version 2.0")
-				.licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE").version("2.0").build();
-	}
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().paths(apiPaths()).build();
+    }
+    private Predicate<String> apiPaths() {
+        return or(regex("/elista/.*"));
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("E-Lista").description("Opis").termsOfServiceUrl("Terms of Service")
+                .contact("Dane kontaktowe").license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE").version("2.0").build();
+    }
 }
